bdba72f19611a4d4ff455fd6aa09f9492ecfa870
Sawansingh24 <119250143+Sawansingh24@users.noreply.github.com>
2023-01-17 16:19:49 +0530

    Update index.py

--- index.py
+++ index.py
@@ -17,7 +17,6 @@ bot = Bot(TOKEN)
 def welcome(update, context) -> None:
     update.message.reply_text(f"Hello {update.message.from_user.first_name}, Welcome To Movie Search Bot.\n"
                               f"🔥 Directly Search From The Bot.")
-                              😎 Made by me @Sawan_singh_24
     update.message.reply_text("👇 Enter Any Movie or Series Name 👇")
 
 
